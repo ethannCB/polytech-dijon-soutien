a6a49d88e4f4728e81f0292f9c8372201b5ca6f6
ethannCB <ethann.campionbaux@gmail.com>
2025-09-17 14:24:50 +0200

    troisieme_fonction_création_code

--- mastermind.py
+++ mastermind.py
@@ -1,3 +1,4 @@
+import random as r
 fr_color=["Rouge","Vert","Bleu","Jaune","Mauve","Noir"]
 color=["Red","Green","Blue","Yellow","Purple","White"]
 
@@ -12,5 +13,15 @@ def create_couleur(l):
         premiere_lettre.append(col[0])
         print(col[0])
     return premiere_lettre 
+
+def create_code(l_color):
+    code=[]
+    for i in range(4):              # pour plus tard changer ici pour modifier le nombre de couleur du code final
+        code.append(l_color[r.randint(0,len(l_color)-1)])   #ajoute a la liste code une couleur  au hazard 
+    return code
+
+def main():
+
+
 
 print(create_couleur(color))
