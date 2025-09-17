bd7144fd193ad14c4ff593a356bbbc59524e5e21
ethannCB <ethann.campionbaux@gmail.com>
2025-09-17 14:00:02 +0200

    première_fonction_couleur

--- mastermind.py
+++ mastermind.py
@@ -1,1 +1,10 @@
-print("bonjou")
+fr_color=["Rouge","Vert","Bleu","Jaune","Mauve","Noir"]
+color=["Red","Green","Blue","Yellow","Purple","White"]
+
+def couleur(liste):
+    print("Voici les couleurs possible :")
+    for color in liste:
+        print("-",color)
+        #print("\n")
+
+couleur(fr_color)
