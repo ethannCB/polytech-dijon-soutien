afc040f28be1d33c3ce0fe8d78f1384cead77081
ethannCB <ethann.campionbaux@gmail.com>
2025-09-17 14:52:25 +0200

    test_pour_que_ca_marche

--- mastermind.py
+++ mastermind.py
@@ -14,7 +14,7 @@ def create_couleur(l):
         print(col[0])
     return premiere_lettre 
 
-def create_code(l_color):
+def create_code(l_color):    #mettre liste avec seulement les premiere lettres
     code=[]
     for i in range(4):              # pour plus tard changer ici pour modifier le nombre de couleur du code final
         code.append(l_color[r.randint(0,len(l_color)-1)])   #ajoute a la liste code une couleur  au hazard 
@@ -25,9 +25,32 @@ def choix_color():
     choix=int(input("Indiquez 1 pour Francais et 2 pour Anglais: "))
     if choix==1: return fr_color
     else: return color
+
+def test(val,code):
+    correct=0
+    partiel=0
+    i=0
+    pop=[]                        #retiens les indices qu'on va pop
+    while i<len(code):
+        #print(i)*
+        print(code[i],val[i])
+        if code[i]==val[i]:
+            print("ok")
+            print(code,val)
+            correct+=1
+            code.pop(i)
+            val.pop(i)
+        else:
+            i+=1
+    #for j in range(len(code)):
+
+
+
+
+
 #def main():
 
 
 
 
-print(choix_color())
+test(["A","B","C","D"],["A","B","D","C"])
